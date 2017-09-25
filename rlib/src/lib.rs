d2415163e524ca59013bba755c062a32361a0c5e
Andreas C. Osowski <andreas@osowski.de>
2017-09-25 18:34:12 +0200

    feature-gate iota.rs and mam rlib dependencies

--- rlib/src/lib.rs
+++ rlib/src/lib.rs
@@ -4,13 +4,21 @@
 extern crate alloc;
 extern crate cty;
 
+#[cfg(feature = "mam")]
 extern crate iota_mam_bindings;
+
+#[cfg(feature = "iotars")]
 extern crate iota_bindings;
+
 extern crate iota_bindings_shared as shared;
 
 pub mod ctrits;
 
+#[cfg(feature = "mam")]
 pub use iota_mam_bindings::*;
+
+#[cfg(feature = "iotars")]
 pub use iota_bindings::*;
+
 pub use ctrits::*;
 

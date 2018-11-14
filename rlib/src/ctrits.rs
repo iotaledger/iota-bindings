791ecfad31f65f00c5dbe9a5a67d2a3b9cf96fff
Andreas C. Osowski <andreas@osowski.de>
2018-11-14 13:30:22 +0100

    Further nightly updates (extern "C")

--- rlib/src/ctrits.rs
+++ rlib/src/ctrits.rs
@@ -1,25 +1,25 @@
 use core;
 use alloc::boxed::Box;
-use alloc::String;
-use alloc::Vec;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 use cty::*;
 use shared::*;
 
 #[no_mangle]
-pub fn iota_ctrits_drop(ctrits: *mut CTrits) {
+pub extern "C" fn iota_ctrits_drop(ctrits: *mut CTrits) {
     let ct = unsafe { Box::from_raw(ctrits) };
     unsafe { Vec::from_raw_parts(ct.data as *mut u8, ct.byte_length, ct.byte_length)};
 }
 
 #[no_mangle]
-pub fn iota_ctrits_convert(ctrits: &CTrits, to: TritEncoding) -> *const CTrits {
+pub extern "C" fn iota_ctrits_convert(ctrits: &CTrits, to: TritEncoding) -> *const CTrits {
     let out = Box::new(ctrits_convert(ctrits, to));
     Box::into_raw(out)
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_from_trytes(ptr: *const u8, len: usize) -> *const CTrits {
+pub extern "C" fn iota_ctrits_ctrits_from_trytes(ptr: *const u8, len: usize) -> *const CTrits {
     let slice: &[u8] = unsafe { core::slice::from_raw_parts(ptr, len) };
     let string = String::from_utf8(slice.to_vec()).ok().unwrap();
     let out = Box::new(ctrits_from_trytes(string));
@@ -28,14 +28,14 @@ pub fn iota_ctrits_ctrits_from_trytes(ptr: *const u8, len: usize) -> *const CTri
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_from_trits(ptr: *const i8, len: usize) -> *const CTrits {
+pub extern "C" fn iota_ctrits_ctrits_from_trits(ptr: *const i8, len: usize) -> *const CTrits {
     let slice: &[i8] = unsafe { core::slice::from_raw_parts(ptr, len) };
     let out = Box::new(ctrits_from_trits(slice.to_vec()));
     Box::into_raw(out)
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_from_bytes(trit_len: usize, ptr: *const u8, len: usize) -> *const CTrits {
+pub extern "C" fn iota_ctrits_ctrits_from_bytes(trit_len: usize, ptr: *const u8, len: usize) -> *const CTrits {
     let slice: &[u8] = unsafe { core::slice::from_raw_parts(ptr, len) };
     let out = Box::new(ctrits_from_bytes(trit_len, slice.to_vec()));
     Box::into_raw(out)
@@ -47,21 +47,21 @@ pub unsafe fn iota_ctrits_trits_to_trytes_inplace(ctrits: &mut CTrits) {
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_encoding(ctrits: &CTrits) -> TritEncoding {
+pub extern "C" fn iota_ctrits_ctrits_encoding(ctrits: &CTrits) -> TritEncoding {
     ctrits.encoding.clone()
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_length(ctrits: &CTrits) -> usize {
+pub extern "C" fn iota_ctrits_ctrits_length(ctrits: &CTrits) -> usize {
     ctrits.length
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_data(ctrits: &CTrits) -> *mut c_void {
+pub extern "C" fn iota_ctrits_ctrits_data(ctrits: &CTrits) -> *mut c_void {
     ctrits.data
 }
 
 #[no_mangle]
-pub fn iota_ctrits_ctrits_byte_length(ctrits: &CTrits) -> usize {
+pub extern "C" fn iota_ctrits_ctrits_byte_length(ctrits: &CTrits) -> usize {
     ctrits.byte_length
 }
